8e252c272bf789541a754fc1bc2ab9b2dba27903
tfkhdyt <tfkhdyt@pm.me>
2022-04-16 17:20:45 +0700

    fix ormconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
-import { config } from '../ormconfig'
+import config from '../ormconfig'
 import { ConfigModule } from '@nestjs/config'
 import { PetsModule } from './pets/pets.module'
 
